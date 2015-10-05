10f53c1d1bb5f5a87d1cdd8b2f6028454248fce6
Daniel Muckerman <danielmuckerman@me.com>
2015-10-04 22:50:47 -0400

    Stubbed out remaning Twitch API methods

--- TwitchApi.go
+++ TwitchApi.go
@@ -8,3 +8,68 @@ func (api *TwitchApi) getChannel(channel string) string {
 	// TODO: implement
 	return ""
 }
+
+func (api *TwitchApi) getChannelVideos(channel string) string {
+	// TODO: implement
+	return ""
+}
+
+func (api *TwitchApi) getChannelFollows(channel string) string {
+	// TODO: implement
+	return ""
+}
+
+func (api *TwitchApi) getChannelTeams(channel string) string {
+	// TODO: implement
+	return ""
+}
+
+func (api *TwitchApi) getChannelBadges(channel string) string {
+	// TODO: implement
+	return ""
+}
+
+func (api *TwitchApi) getEmotes() string {
+	// TODO: implement
+	return ""
+}
+
+func (api *TwitchApi) getUserFollows(user string) string {
+	// TODO: implement
+	return ""
+}
+
+func (api *TwitchApi) isUserFollowing(user string, target string) string {
+	// TODO: implement
+	return ""
+}
+
+func (api *TwitchApi) getGames() string {
+	// TODO: implement
+	return ""
+}
+
+func (api *TwitchApi) searchChannels(query string) string {
+	// TODO: implement
+	return ""
+}
+
+func (api *TwitchApi) searchStreams(query string) string {
+	// TODO: implement
+	return ""
+}
+
+func (api *TwitchApi) searchGames(query string) string {
+	// TODO: implement
+	return ""
+}
+
+func (api *TwitchApi) getStream(query string) string {
+	// TODO: implement
+	return ""
+}
+
+func (api *TwitchApi) getFeaturedStreams() string {
+	// TODO: implement
+	return ""
+}
